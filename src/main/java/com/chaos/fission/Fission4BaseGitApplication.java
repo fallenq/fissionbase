a10212391265d26992ac38a1464d860ff35cffd8
jiaolongtian <haiyan.xu.vip@gmail.com>
2018-06-13 09:48:25 +0800

    *

---
+++ src/main/java/com/chaos/fission/Fission4BaseGitApplication.java
@@ -0,0 +1,12 @@
+package com.chaos.fission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fission4BaseGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fission4BaseGitApplication.class, args);
+	}
+}
