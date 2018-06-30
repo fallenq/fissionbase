bb03a241a33e0642240a85cb7fc9e7f88c7da9cd
jiaolongtian <haiyan.xu.vip@gmail.com>
2018-06-30 15:31:53 +0800

    *

--- src/main/java/com/chaos/fission/Fission4BaseApplication.java
+++ src/main/java/com/chaos/fission/Fission4BaseApplication.java
@@ -2,11 +2,12 @@ package com.chaos.fission;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 import com.chaos.fission.config.UploadPropertis;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 @EnableConfigurationProperties({UploadPropertis.class})
 public class Fission4BaseApplication {
 
